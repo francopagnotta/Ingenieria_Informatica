ea9c19a3b0536a46ea0089201b84b4a186bf1347
francopagnotta <francoapagnotta@gmail.com>
2024-04-13 04:51:44 -0300

    add function to restart the game

--- pensamiento_computacional/session_3/excersise_5.py
+++ pensamiento_computacional/session_3/excersise_5.py
@@ -13,3 +13,23 @@ def getUserChoce():
 def showFeedback(user_choice, game_choice):
   print(f"You chose: {options[user_choice]}!")
   print(f"I chose: {options[game_choice]}! I win!")
+
+# le consulta al usuario si quiere volver a jugar o abandonar el juego
+def restart():
+  restart = True
+
+  while True:
+    print("\n" + "Do you want to play again?" + "\n" + "1: YES" + "\n" + "2: NO" + "\n")
+
+    user_choice_restart = int(input())
+
+    if user_choice_restart == 1:
+      break
+    elif user_choice_restart == 2:
+      restart = False
+      break
+    else:
+      print("Please enter a valid option")
+      continue
+
+  return restart
